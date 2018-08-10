2b25be73b52125356d18deef9c1c3782925b87d2
Mark Harfouche <mark.harfouche@gmail.com>
2018-08-09 21:19:31 -0400

    Remove the duplicate documentation

--- docs/examples.rst
+++ docs/examples.rst
@@ -1,14 +1,6 @@
 Usage examples
 ==============
 
-What the documentation will look like
--------------------------------------
-
-One important advantage of using these decorators is that they ensure that the
-docstrings are correct. This module has as hypothetical version number of
-``0.14``. Click on the source links to see what the source looks like for each
-of these functions.
-
 .. automodule::  examples.kwarg_default_deprecation
     :members:
     :undoc-members:
--- examples/kwarg_default_deprecation.py
+++ examples/kwarg_default_deprecation.py
@@ -1,10 +1,14 @@
-"""Example of how to use the deprecaction factory in your own library.
+"""
+What the documentation will look like
+-------------------------------------
 
-As a developer, you should inspect the source of this file.
+One important advantage of using these decorators is that they ensure that the
+docstrings are correct. This module has as hypothetical version number of
+``0.14``. Click on the source links to see what the source looks like for each
+of these functions.
 
 Look at the doc page to see how your users would see your documetnation.
 
-I assume that this package has a version number '0.14'
 """
 
 from deprecation_factory import default_parameter_change
