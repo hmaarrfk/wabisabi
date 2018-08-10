0d083dc42c4066b3489cfd4efa3f933cc8e390a6
Mark Harfouche <mark.harfouche@gmail.com>
2018-08-09 20:34:34 -0400

    Test the numpydocs

--- deprecation_factory/tests/test_kwarg_deprecation.py
+++ deprecation_factory/tests/test_kwarg_deprecation.py
@@ -4,6 +4,7 @@ import warnings
 import inspect
 import pytest
 from pytest import warns
+from numpydoc.docscrape import FunctionDoc
 
 
 parametrize = pytest.mark.parametrize
@@ -66,7 +67,6 @@ def test_deprecating2():
         expeccted_warning_lineno = inspect.currentframe().f_lineno + 1
         assert foo2_0_15('a') == 'aBC'
 
-    #import pdb; pdb.set_trace()
     assert len(record) == 1
     the_warning = record[0]
     assert expeccted_warning_lineno == the_warning.lineno
@@ -123,3 +123,69 @@ def test_foo2_signature():
     sig_13 = inspect.signature(foo2_0_13)
     assert sig_13.parameters['b'].default == 'b'
     assert sig_13.parameters['c'].default == 'c'
+
+
+def foo_with_docs(bar='hello'):
+    """This is a function foo!
+
+    It has some docs, isn't this cool!
+
+    Parameters
+    ----------
+    bar: str
+        This is a parameter.
+
+    """
+    return bar
+
+
+foo_with_docs_15 = default_parameter_change('0.15', bar='world')(foo_with_docs)
+foo_with_docs_13 = default_parameter_change('0.13', bar='world')(foo_with_docs)
+
+
+def test_foo_docs():
+    assert 'Warns' in foo_with_docs_15.__doc__
+    docs = FunctionDoc(foo_with_docs_15)
+    assert 'Warns' in docs
+    assert 'FutureWarning' == docs['Warns'][0][0]
+    assert "bar : 'world' -> 'hello'" in '\n'.join(docs['Warns'][0][2])
+
+    assert foo_with_docs_13.__doc__ == foo_with_docs.__doc__
+
+    with warns(FutureWarning, match='In release 0.15 of mylib'):
+        assert foo_with_docs_15() == 'world'
+
+
+def foo2_with_docs(bar='hello', baz='world'):
+    """This is a function foo!
+
+    It has some docs, isn't this cool!
+
+    Parameters
+    ----------
+    bar: str
+        This is a parameter.
+
+    """
+    return bar + ' ' + baz
+
+
+foo2_with_docs_15 = default_parameter_change(
+    '0.15', bar='bonjour', baz='monde')(foo2_with_docs)
+foo2_with_docs_13 = default_parameter_change(
+    '0.13', bar='bonjour', baz='monde')(foo2_with_docs)
+
+
+def test_foo2_docs():
+    assert 'Warns' in foo2_with_docs_15.__doc__
+    docs = FunctionDoc(foo2_with_docs_15)
+    assert 'Warns' in docs
+    assert 'FutureWarning' == docs['Warns'][0][0]
+    assert "bar : 'bonjour' -> 'hello'" in '\n'.join(docs['Warns'][0][2])
+    assert "baz : 'monde' -> 'world'" in '\n'.join(docs['Warns'][0][2])
+
+    assert foo2_with_docs_13.__doc__ == foo2_with_docs.__doc__
+
+    with warns(FutureWarning, match='In release 0.15 of mylib'):
+        assert foo2_with_docs_15() == 'bonjour monde'
+    assert foo2_with_docs_13() == 'hello world'
